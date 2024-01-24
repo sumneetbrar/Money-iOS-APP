1f3c00b135a9ec6b2cece3d2518a90175b408188
sumneetbrar <sumneetbrar18@gmail.com>
2024-01-23 22:34:47 -0800

    Initial Commit

---
+++ Money/MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyApp.swift
+//  Money
+//
+//  Created by Sumneet Brar on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
